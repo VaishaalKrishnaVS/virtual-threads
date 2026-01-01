6f3c2c486cd81c0450bf71de48e62f2221dc1664
Vaishaal Krishna VS <vaishaalinc@gmail.com>
2026-01-01 15:03:43 +0530

    feat: creating threads using builder

--- vt-playground/src/main/java/pg/vt/sec01/IOTaskDemo.java
+++ vt-playground/src/main/java/pg/vt/sec01/IOTaskDemo.java
@@ -1,9 +1,11 @@
 package pg.vt.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class IOTaskDemo {
-    public static final int MAX_ITR = 50_000_000;
-    public static void main(String[] args) {
-        platformThreadDemo();
+    public static final int MAX_ITR = 10;
+    public static void main(String[] args) throws InterruptedException {
+        platformDaemonThreadDemoUsingBuilder();
     }
     private static void platformThreadDemo() {
         for (int i = 0; i < MAX_ITR; i++) {
@@ -11,5 +13,26 @@ public class IOTaskDemo {
             Thread thread = new Thread(() -> Task.someIntensiveIOTask(j));
             thread.start();
         }
+    }
+    private static void platformThreadDemoUsingBuilder(){
+        var builder = Thread.ofPlatform().name("vt-platform");
+        for (int i = 0; i < MAX_ITR; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(()->Task.someIntensiveIOTask(j));
+            thread.start();
+        }
+    }
+    private static void platformDaemonThreadDemoUsingBuilder() throws InterruptedException {
+        var builder = Thread.ofPlatform().daemon().name("vtd-platform");
+        var latch = new CountDownLatch(MAX_ITR);
+        for (int i = 0; i < MAX_ITR; i++) {
+            int j = i;
+            Thread thread = builder.unstarted(()-> {
+                Task.someIntensiveIOTask(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
     }
 }
