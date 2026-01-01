0d89bade7f6bd3ef36e7c6a0ea25ec673d0ec89f
Vaishaal Krishna VS <vaishaalinc@gmail.com>
2026-01-01 20:05:46 +0530

    feat: create virtual threads

--- vt-playground/src/main/java/pg/vt/sec01/IOTaskDemo.java
+++ vt-playground/src/main/java/pg/vt/sec01/IOTaskDemo.java
@@ -4,8 +4,9 @@ import java.util.concurrent.CountDownLatch;
 
 public class IOTaskDemo {
     public static final int MAX_ITR = 10;
+    public static final int MAX_VIR_ITR = 100_000;
     public static void main(String[] args) throws InterruptedException {
-        platformDaemonThreadDemoUsingBuilder();
+        virtualThreadDemo();
     }
     private static void platformThreadDemo() {
         for (int i = 0; i < MAX_ITR; i++) {
@@ -28,6 +29,19 @@ public class IOTaskDemo {
         for (int i = 0; i < MAX_ITR; i++) {
             int j = i;
             Thread thread = builder.unstarted(()-> {
+                Task.someIntensiveIOTask(j);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
+    }
+    private static void virtualThreadDemo() throws InterruptedException {
+        var builder = Thread.ofVirtual().name("vt-virtual-",1);
+        var latch = new CountDownLatch(MAX_VIR_ITR);
+        for (int i = 0; i < MAX_VIR_ITR; i++) {
+            int j = i;
+            var thread = builder.unstarted(()->{
                 Task.someIntensiveIOTask(j);
                 latch.countDown();
             });
