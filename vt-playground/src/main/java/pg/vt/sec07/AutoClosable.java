ea5ac5a48a7b4920a8983ed0c7d24c84876e3928
Vaishaal Krishna VS <vaishaalinc@gmail.com>
2026-01-06 15:06:00 +0530

    feat: autoClosable implementation

---
+++ vt-playground/src/main/java/pg/vt/sec07/AutoClosable.java
@@ -0,0 +1,26 @@
+package pg.vt.sec07;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pg.vt.util.CommonUtils;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+
+public class AutoClosable {
+    private static final Logger log = LoggerFactory.getLogger(AutoClosable.class);
+    public static void main(String[] args) {
+        log.info("Main:: Thread: {}",Thread.currentThread());
+        try(var executorService = Executors.newFixedThreadPool(3)) {
+            executorService.submit(AutoClosable::someTask);
+            executorService.submit(AutoClosable::someTask);
+            executorService.submit(AutoClosable::someTask);
+            executorService.submit(AutoClosable::someTask);
+            executorService.submit(AutoClosable::someTask);
+        }
+    }
+    private static void someTask(){
+        CommonUtils.sleep(Duration.ofSeconds(2));
+        log.info("Thread: {}", Thread.currentThread());
+    }
+}
